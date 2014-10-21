380df13d64efabe2c1de57488ec5418b90af6010
nateradtke <nate@catalyze.io>
2014-10-21 14:21:59 -0500

    Added test Sidekiq task

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,6 @@
 class WelcomeController < ApplicationController
+
+    def index
+        TestWorker.perform_async()
+    end
 end
---
+++ app/workers/test_worker.rb
@@ -0,0 +1,8 @@
+class TestWorker
+  include Sidekiq::Worker
+
+  def perform
+    puts 'Hello, World'
+  end
+end
+
